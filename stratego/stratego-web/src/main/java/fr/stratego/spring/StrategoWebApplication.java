c5b49283b09bdf6c2eae12eb6cb615b9fb0cd37d
caso224 <casocamara@hotmail.com>
2017-07-17 20:46:04 +0200

    config -  architecture du projet

---
+++ stratego/stratego-web/src/main/java/fr/stratego/spring/StrategoWebApplication.java
@@ -0,0 +1,12 @@
+package fr.stratego.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategoWebApplication.class, args);
+	}
+}
